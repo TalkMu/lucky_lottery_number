22f0fa03d53f1fb561c45617123eca8a1ccd075b
YangZhuang <koolss@koolss.com>
2022-03-23 11:46:17 +0800

    初始化项目

---
+++ src/pages/index/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <view class="container">
+    <view class="title-container">
+      <text class="title">第2022023期</text>
+      <text class="title">吉祥号值得来一套</text>
+      <view class="detail">
+        <text>每周二、四、日开奖</text>
+        <text>20:00 停售，21:15 开奖</text>
+      </view>
+    </view>
+    <view class="number-container">
+      <view class="number-item" v-for="n in 12">
+        <text class="title">虎虎生威:</text>
+        <text class="number">12</text>
+        <text class="number">12</text>
+        <text class="number">12</text>
+        <text class="number">12</text>
+        <text class="number">12</text>
+        <text class="number">12</text>
+        <text class="number">12</text>
+        <text class="number">12</text>
+        <text class="number">12</text>
+        <text class="number">12</text>
+      </view>
+    </view>
+  </view>
+</template>
+
+<script>
+import "./index.scss";
+
+export default {
+  data() {
+    return {
+      msg: "Hello world!",
+    };
+  },
+};
+</script>
